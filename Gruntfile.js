85c41f19247c25f364e38340b67a689b605a591c
szenadam <szenadam1@gmail.com>
2019-05-11 18:30:28 +0200

    Configure grunt to build project

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,19 +7,30 @@ module.exports = function(grunt) {
           style: "expanded"
         },
         files: {
-          "dist/css/style.css": "css/style.scss",
+          "src/css/style.css": "src/css/style.scss"
         }
       }
     },
     copy: {
       main: {
         files: [
+          { expand: true, cwd: "src/", src: ["*.html"], dest: "dist/", filter: "isFile" },
+          { expand: true, cwd: "src/", src: ["assets/**"], dest: "dist/" },
+          { expand: true, cwd: "src/", src: ["css/style.css"], dest: "dist/"},
+          { expand: true, cwd: "src/", src: ["js/**"], dest: "dist/" }
         ]
+      }
+    },
+    clean: {
+      build: {
+        src: ['dist']
       }
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-sass");
 
-  grunt.registerTask("default", ["sass"]);
+  grunt.registerTask("default", ["clean", "sass", "copy"]);
 };
